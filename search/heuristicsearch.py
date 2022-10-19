39a805725e6f2b0506b16217658f8193c5fe025d
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-19 10:02:39 -0700

    Update heuristicsearch.py
    added comments. refactored to make more readable and follow style guide.
    fixed best_cost -> path_cost bug

--- search/heuristicsearch.py
+++ search/heuristicsearch.py
@@ -2,7 +2,7 @@
 from collections import defaultdict
 from math import inf
 from heapdict import heapdict
-def search(start, goal, edges, heuristic=lambda:0):
+def search(start, goal, edges, heuristic=lambda x: 0):
     """
     search finds the best path between the start and goal. If there is
     no path, return None.
@@ -16,21 +16,22 @@ def search(start, goal, edges, heuristic=lambda:0):
         a heuristic should be admissible:
             best_cost[node] + heuristic(node) < best_cost[goal]
         where best_cost is the sum of the weights from start.
-        A heuristic is consistent if for every vertex:
-            for neighbor, weight in edges(vertex):
+        A heuristic is consistent if for every vertex and every
+        (neighbor, weight) in edges(vertex):
                 heuristic(node) >= heuristic(neighbor) + weight
         If a heuristic is also consistent, search will find the optimal
         path without expanding a vertex more than once.
-    
     """
-    
+    # came_from: vertex -> previous vertex of best path to vertex
     came_from = dict()
-    path_cost = defaultdict(lambda : inf)
-    
-    # Setup queue.
+    # path_cost: vertex -> sum of edge weights of best path to vertex
+    path_cost = defaultdict(lambda: inf)
+    # open_vertices: vertex -> priority of that vertex
     open_vertices = heapdict()
+    
+    # Add the start vertex to the queue
     open_vertices[start] = 0
-    best_cost[start] = 0
+    path_cost[start] = 0
     
     while open_vertices:
         current, priority = open_vertices.popitem()
@@ -44,10 +45,10 @@ def search(start, goal, edges, heuristic=lambda:0):
             return path
         for neighbor, weight in edges(current):
             # current_cost is the distance to neighbor through current.
-            current_cost = best_cost[opened_node] + weight 
-            if current_cost < best_cost[neighbor]:
+            current_cost = path_cost[opened_node] + weight 
+            if current_cost < path_cost[neighbor]:
                 best_cost[neighbor] = current_cost
                 open_vertices[neighbor] = current_cost + heuristic(neighbor)
     else:
-        # If the queue runs out, return no path.
+        # If the queue runs out of vertices, return no path.
         return None
