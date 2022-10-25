2642e75051080707cfb51077befb08fc2c7889fb
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-25 12:54:43 -0700

    Update erv.py
    finished algorithm

--- vote/erv.py
+++ vote/erv.py
@@ -2,35 +2,38 @@ def elect(ballot_box, valid_candidates):
     """
     An alternative to Instant-Runoff Voting, this algorithm eliminates the candidate with the most lowest rank votes, then rechecks the remaining candidates
     
+    Returns majority winner or (majority winner
+    
     Arugments:
     ballot_box: a list of ordered lists of candidates
     candidates: the list of valid candidates
     """
     eliminated = set()
+    to_win = len(ballot_box) // 2
+    majority_winner = None
     while ballot_box:
         # Counting Ballots
-        count = dict.fromkeys(valid_candidates, 0)
+        count = defaultdict(int)
         for ballot in ballot_box:
+            assert set(ballot) == set(valid_candidates)
             for candidate in ballot:
                 if candidate not in eliminated:
                     count[candidate] += 1
                     break
-        sorted_candidates = [c in sorted(count, key=lambda c: count[c])]
-        ### fix below this
-        if len(sorted_candidates) == 1:
-            return sorted_candidates[0]
+                    
+        ranked = [c in sorted(count, key=lambda c: count[c])]
+        round_winner = ranked[0]
+        if count[round_winner] > to_win:
+            majority_winner = round_winner
+        if len(ranked) == 1:
+            elimination_winner = round_winner
+            return majority_winner, elimination_winner
         
-        # Candidate Elimination
-        for ranking, candidate in enumerate(sorted_candidates):
-            # Greedy Elimination
-            aggregate = sum([count[c] for c in sorted_candidates[ranking+1:]])
-            if count[candidate] > aggregatet:
-                next_eliminated = set(sorted_candidates[ranking+1:])
-                break
-        else:
-            # Single Elimination
-            fewest_votes = min(count.values())
-            next_eliminated = {c for c in count if count[c] == fewest_votes}
-            if len(next_eliminated) != 1:
-                return NotImplemented, "This ballot_box has an unhandled tie"
-        eliminated |= next_eliminated
+        fewest_votes = min(count.values())
+        round_losers = {c for c in count if count[c] == fewest_votes}
+        if len(round_losers) == 1:
+            eliminated |= round_losers
+        if majority_winner:
+            return majority_winner, None
+        if len(round_losers) != 1:
+            raise NotImplemented, "This ballot_box has an unhandled tie."
