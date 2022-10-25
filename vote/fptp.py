0358831a1f654fc890090a9159e9552696660ca2
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-25 07:50:20 -0700

    Update fptp.py
    wrote algorithm

--- vote/fptp.py
+++ vote/fptp.py
@@ -1,1 +1,13 @@
-
+def elect(ballot_box):
+    """
+    An algorithm for calculating the first-past-the-post (FPTP) winner.
+    
+    Arugments:
+    ballot_box: an iterable of ordered lists of candidates
+    """
+    count = defaultdict(int)
+    for ballot in ballot_box:
+        count[ballot] += 1
+    winners = {c for c in count if count[c] == max(count.values())}
+    if len(winners) == 1:
+        return winners[0]
