ddc27ed54b605379409c91c545a1f72ca3b65f32
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-25 12:39:07 -0700

    Update irv.py
    fixed syntax error. changed some variable names

--- vote/irv.py
+++ vote/irv.py
@@ -28,21 +28,22 @@ def elect(ballot_box):
             else:
                 # remove empty ballots
                 ballot_box.remove(ballot)
-        sorted_candidates = [c in sorted(count, key=lambda c: count[c])]
-        if len(sorted_candidates) == 1:
-            return sorted_candidates[0]
         
-        # Candidate Elimination
-        for ranking, candidate in enumerate(sorted_candidates):
+        ranked_candidates = [c in sorted(count, key=lambda c: count[c])]
+        round_winner = ranked[0]
+        if len(ranked_candidates) == 1:
+            return round_winner
+        
+        for ranking, candidate in enumerate(ranked_candidates):
             # Greedy Elimination
-            aggregate = sum([count[c] for c in sorted_candidates[ranking+1:]])
-            if count[candidate] > aggregatet:
-                next_eliminated = set(sorted_candidates[ranking+1:])
+            aggregate = sum([count[c] for c in ranked_candidates[ranking+1:]])
+            if count[candidate] > aggregate:
+                losers = set[ranked_candidates[ranking+1:])
                 break
         else:
             # Single Elimination
             fewest_votes = min(count.values())
-            next_eliminated = {c for c in count if count[c] == fewest_votes}
-            if len(next_eliminated) != 1:
-                return NotImplemented, "This ballot_box has an unhandled tie"
-        eliminated |= next_eliminated
+            losers = {c for c in count if count[c] == fewest_votes}
+            if len(losers) != 1:
+                raise NotImplemented, "This ballot_box has an unhandled tie"
+        eliminated |= losers
