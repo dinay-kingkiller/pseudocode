4f89fa19939efe6e87d1b253fbc449def9097035
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-27 15:29:50 -0700

    Update pheromone.py
    more loops

--- search/pheromone.py
+++ search/pheromone.py
@@ -1,3 +1,12 @@
-def search(start, goal, edges, heuristic=lambda x: 0):
-    while True:
-        break
+from heapdict import heapdict
+swarm_size = 500
+predict_parameter = 1 #>=1
+measure_parameter = 0
+def search(start, goal, edges):
+    swarm = [start for i in range(swarm_size)]
+    for ant in swarm:
+        for next_state, next_weight in edges(ant):
+            pass
+        
+        
+        
