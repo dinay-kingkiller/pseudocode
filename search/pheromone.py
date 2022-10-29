dc51deb3ecbbf872ee208cdfd6ecee64afbb2a22
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-29 00:08:51 -0700

    Update pheromone.py
    copied code written on airplane. added a bug via insert mode...

--- search/pheromone.py
+++ search/pheromone.py
@@ -1,46 +1,35 @@
-from collections import defaultdict
-from heapdict import heapdict
 from random import choose
-distance_power = 1 #>=1
-pheromone_power = 0 #>=0
-reward_power = 0
-decay_power = 0
-def search(start, goal, edges, distance, total_ants):
-    """
-    edges: state -> next_state
-    """
-    weighted_pheromone = defaultdict(int)
-    ant_count = defaultdict(int)
-    vertices = [start]
-    ant_count[start] = total_ants
-    for _ in range(10):
-        for current in vertices:
-            if ant_count[current] == 0:
+
+def search(start, goal, edges, distance, distance_power=1, pheromone_power=0, evaporation_power=0, reward_power=1):
+    d
+    pheromone = dict.fromkeys(edges, 0)
+    pheromone_weight = dict.fromkeys(edges, 1)
+    distance_weight = {edge: 1/distance(edge)^distance_power for edge in edges}
+    desidict(distance_weight)
+
+    neighbors = dict()
+    for current, _ in edges:
+        neighbors[current] = [edge for edge in edges if current == edge[0]]
+    swarm = range(ant_count)
+
+    while exploring:
+        # move ants
+        for ant in swarm
+            next_edges = neighbors[location[ant]]
+            possibilities = [likelihood[edge] for edge in next_edges]
+            next_edge = choose(next_edges, weight=possibilities)
+            location[ant] = next_edge[1]
+            tour[ant].append(next_edge)
+            tour_length[ant] += distance[next_edge]
+        for edge in edges:
+            pheromone[edge] *= 1 - evaporation_power
+        for ant in swarm
+            if location[ant] != goal:
                 continue
-            likelihood = [0] * len(edges)
-            for i, neighbor in enumerate(edges(current)):
-                if (current, neighbor) not in weighted_distance:
-                    weighted_distance[(current, neighbor)] = 1/distance[(current, neighbor)]**distance_power
-                likelihood[i] = weighted_pheromone[(ant, neighbor)] * weighted_distance[(ant, neighbor)])
-            next_vertices = choose(edges(current), weight=likelihood, k=)
-            for neighbor in next_vertices:
-                ant_count[neighbor] += 1
-            ant_count[current] = 0
-    ##
-        for ant in swarm:
-            path[ant].append(current)
-            likelihood = []
-            for neighbor, distance in edges(current):
-                if (current, neighbor) not in weighted_distance:
-                    weighted_distance[(current, neighbor)] = 1/distance**distance_power
-                likelihood.append(weighted_pheromone[(ant, neighbor)] * weighted_distance[(ant, neighbor)])
-            next_node = choose(edges(ant), weight=likelihood)
-        
-        # ants move
-        # ants talk
-        
-            
-            
-        
-        
-        
+            for edge in path:
+                pheromone[edge] += deposit_power / tour_length[ant]
+            location[ant] = start
+            tour = [start]
+        for edge in edges:
+            pheromone_weight[edge] = pheromone[edge]**pheromone_power
+            likelihood[edge] = 
