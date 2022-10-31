4a48824a9c4cd71a33e8ea1f1559611ba500f102
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-10-31 07:40:56 -0700

    Update pheromone.py
    added a docstring

--- search/pheromone.py
+++ search/pheromone.py
@@ -1,6 +1,22 @@
 from collections import defaultdict
 from random import choose
 def search(start, goal, moves, distance, distance_power=1, pheromones_power=0, evaporation_power=0, reward_power=1):
+    """
+    search tries to constantly improve an optimal path from start to goal.
+    
+    Note: does not currently return anything or halt. It will run forever until broken.
+    
+    Arguments:
+    start - the initial vertex of a graph
+    goal - the vertex on the other side of the path
+    moves - an iterable of tuples (current, neighbor) all possible moves within the graph
+    distance - a map: move -> cost of making that move
+    distance_power (>=1, default=1) - higher values will force ants to try easy moves
+    pheromones_power (>=0, default=0) - higher values will force ants to try their favorite moves
+    evaporation_power (>=0 and <=1, default=0) - how long should pheromones stay on the edges:
+        0 -> pheromones never leave, 1 -> pheromones only affect paths for a single round 
+    reward_power (default=1) - how much pheromone is dropped on reaching a goal
+    """
     pheromones = defaultdict(int)
     swarm = range(ant_count)
     location = [start for ant in swarm]
