db18bae53864ea0ca8eb8b5b5e59328da613c6e5
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-12-02 15:16:31 -0800

    Update bubble-sort.scm

--- sort/bubble-sort.scm
+++ sort/bubble-sort.scm
@@ -1,19 +1,19 @@
 (letrec ((bubble-tail
 	  (lambda (unsorted compare? sorted swap?)
 	    ; compare? is a lambda expression that evaluates true if a value should be before another.
-	    ; swap? is true if a change to the order has happened during the last run through
+	    ; swap? is true if a change to the order has happened during the last pass.
 	    (cond
 	     ((null? unsorted)
 	      ; if two values have been swapped restart otherwise return sorted
 	      (if swap? sorted (bubble-tail (reverse sorted) compare? '() #t)))
 	     ((null? (cdr unsorted))
 	      (bubble-tail (cdr unsorted) compare? (cons (car unsorted) sorted) swap?))
-	     ((compare? (car unsorted) (cadr unsorted))
+	     ((compare? (car unsorted) (cadr unsorted)) ; this isn't clear
 	      (bubble-tail
 	       (cons (car unsorted) (cddr unsorted))
 	       compare?
 	       (cons (cadr unsorted) sorted) #f))
-	     (else ; last value is in correct position
+	     (else ; else if the current value is in the correct position
 	      (bubble-tail (cdr unsorted) compare? (cons (car unsorted) sorted) swap?)))))
 	 (my-bubble-sort (lambda (unsorted compare?) (bubble-tail unsorted compare? '() #t))))
   (newline)
