c8af9e5db2839d4c979edba43b0b6adcdb828d6e
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-12-06 08:44:00 -0800

    Update bubble-sort.scm
    renamed my-bubble-sort -> bubble-sort

--- sort/bubble-sort.scm
+++ sort/bubble-sort.scm
@@ -1,22 +1,22 @@
 (letrec ((bubble-tail
-	  (lambda (unsorted compare? sorted swap?)
-	    ; compare? is a lambda expression that evaluates true if a value should be before another.
-	    ; swap? is true if a change to the order has happened during the last pass.
-	    (cond
-	     ((null? unsorted)
-	      ; if two values have been swapped restart otherwise return sorted
+          (lambda (unsorted compare? sorted swap?)
+            ; compare? is a lambda expression that evaluates true if a value should be before another.   
+            ; swap? is true if a change to the order has happened during the last pass.                  
+            (cond
+             ((null? unsorted)
+              ; if two values have been swapped restart otherwise return sorted                          
 	      (if swap? sorted (bubble-tail (reverse sorted) compare? '() #t)))
-	     ((null? (cdr unsorted))
-	      (bubble-tail (cdr unsorted) compare? (cons (car unsorted) sorted) swap?))
-	     ((compare? (car unsorted) (cadr unsorted)) ; this isn't clear
+             ((null? (cdr unsorted))
+              (bubble-tail (cdr unsorted) compare? (cons (car unsorted) sorted) swap?))
+             ((compare? (car unsorted) (cadr unsorted)) ; this isn't clear                               
 	      (bubble-tail
 	       (cons (car unsorted) (cddr unsorted))
 	       compare?
 	       (cons (cadr unsorted) sorted) #f))
-	     (else ; else if the current value is in the correct position
+             (else ; else if the current value is in the correct position                                
 	      (bubble-tail (cdr unsorted) compare? (cons (car unsorted) sorted) swap?)))))
-	 (my-bubble-sort (lambda (unsorted compare?) (bubble-tail unsorted compare? '() #t))))
+         (bubble-sort (lambda (unsorted compare?) (bubble-tail unsorted compare? '() #t))))
   (newline)
-  (display "(my-bubble-sort '(3 7 8 5 2 1 9 5 4) <)\n")
-  (display (my-bubble-sort '(3 7 8 5 2 1 9 5 4) <))
+  (display "(bubble-sort '(3 7 8 5 2 1 9 5 4) <)\n")
+  (display (bubble-sort '(3 7 8 5 2 1 9 5 4) <))
   (newline))
