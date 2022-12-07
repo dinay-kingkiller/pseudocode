db4f6869b192c6326a401074dbad7d347ae76ed6
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-12-07 08:11:13 -0800

    Update insert-sort.scm
    finished insertion-sort algorithm

--- sort/insert-sort.scm
+++ sort/insert-sort.scm
@@ -1,5 +1,21 @@
-(letrec ((my-insert-sort (lambda (unsorted compare?) #t)))
+(letrec
+    ((insertion-tail
+      (lambda (sorted checked unsorted compare?)
+	(cond
+	 ((null? checked)
+	  (if (null? unsorted)
+	      sorted
+	      (insertion-tail sorted (list (car unsorted)) (cdr unsorted) compare?)))
+	 ((null? sorted)
+	  (insertion-tail (list (car checked)) (cdr checked) unsorted compare?))
+	 ((compare? (car sorted) (car checked))
+	  (insertion-tail (cdr sorted) (cons (car checked) (cons (car sorted) (cdr checked))) unsorted compare?))
+	 (else
+	  (insertion-tail (append (reverse checked) sorted) '() unsorted compare?)))))
+     (insertion-sort
+      (lambda (unsorted compare?)
+	(insertion-tail '() '() unsorted compare?))))
   (newline)
-  (display "(my-insert-sort '(3 7 8 5 2 1 9 5 4) <)\n")
-  (display (my-insert-sort '(3 7 8 5 2 1 9 5 4) <))
+  (display "(insertion-sort '(3 7 8 5 2 1 9 5 4) <)\n")
+  (display (insertion-sort '(3 7 8 5 2 1 9 5 4) <))
   (newline))
