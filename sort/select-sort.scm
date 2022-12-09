00c8e42830512b04860b0a2669e28949d5d573a0
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2022-12-09 10:54:44 -0800

    Update select-sort.scm
    define -> letrec

--- sort/select-sort.scm
+++ sort/select-sort.scm
@@ -1,13 +1,19 @@
-(define (my-select-sort lst fun)
-  (define (select-tail checked unchecked sorted lowest)
-    (cond
-     ((null? unchecked)
-      (if (null? checked)
-	  (cons lowest sorted)
-	  (select-tail '() (cdr checked) (cons lowest sorted) (car checked))))
-     ((> lowest (car unchecked)) (select-tail (cons lowest checked) (cdr unchecked) sorted (car unchecked)))
-     (else (select-tail (cons (car unchecked) checked) (cdr unchecked) sorted lowest))))
-  (if (null? lst) '() (reverse (select-tail '() (cdr lst) '() (car lst)))))
-
-(display "(my-select-sort '(3 7 8 5 2 1 9 5 4) values)\n")
-(display (my-select-sort '(3 7 8 5 2 1 9 5 4) values)) (newline) (newline)
+(letrec
+    ((select-tail
+      (lambda (checked unchecked sorted lowest compare?)
+	(cond
+	 ((null? unchecked)
+	  (if (null? checked)
+	      (cons lowest sorted)
+	      (select-tail '() (cdr checked) (cons lowest sorted) (car checked) compare?)))
+	 ((compare? lowest (car unchecked))
+	  (select-tail (cons lowest checked) (cdr unchecked) sorted (car unchecked) compare?))
+	 (else
+	  (select-tail (cons (car unchecked) checked) (cdr unchecked) sorted lowest compare?)))))
+     (select-sort
+      (lambda (lst compare?)
+	(if (null? lst) '() (select-tail '() (cdr lst) '() (car lst) compare?)))))
+  (newline)
+  (display "(select-sort '(3 7 8 5 2 1 9 5 4) <)\n")
+  (display (select-sort '(3 7 8 5 2 1 9 5 4) <))
+  (newline))
